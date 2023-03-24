4ea0f9471f4efeed53d4dec6dab271c66ce8da8d
matsushimen <ukasustam@gmail.com>
2023-03-24 14:05:48 +0000

    fix

--- src/disk.zig
+++ src/disk.zig
@@ -1,95 +1,81 @@
 const std = @import("std");
 const File = std.fs.File;
-pub const PAGE_SIZE: u64 = 4096;
+const testing = std.testing;
+const assert = testing.assert;
+const expectEqual = testing.expectEqual;
 
-pub const Page = struct{
-    id: u64,
-};
+const PAGE_SIZE: usize = 4096;
 
-fn createOrOpen(heap_file_path: []const u8) File.OpenError!File {
-    return std.fs.cwd().openFile(heap_file_path, .{}) catch |err| {
-        if(err == File.OpenError.FileNotFound) {
-            return std.fs.cwd().createFile(heap_file_path, .{});
-        }
-        else {
-            return err;
-        }
-    };
-}
+const DiskManager = struct {
+    pub const Self = @This();
 
-
-pub const DiskManager = struct
-{
     heap_file: File,
+    next_page_id: usize,
 
-    next_page: u64,
+    pub fn open(heap_file_path: []const u8) !Self {
+        var heap_file = try std.fs.cwd().createFile(heap_file_path, .{});
 
-    const Self = @This();
-
-
-    
-
-    pub fn open(heap_file_path: []const u8) File.OpenError!Self {
-        var heap_file = try createOrOpen(heap_file_path);
-        defer heap_file.close();
-        return Self.new(heap_file);
+        return Self {
+            .heap_file = heap_file,
+            .next_page_id = 0,
+        };
     }
 
-    pub fn new(heap_file: File) !Self {
-
-        // ファイルサイズを取得
-        const heap_file_size: u64 = (try heap_file.stat()).size;
-        var next_page_id: u64 = heap_file_size/PAGE_SIZE;
-
-        return Self{.heap_file=heap_file, .next_page=next_page_id };
-
+    pub fn allocate_page(self: *Self) !usize {
+        const page_id = self.next_page_id;
+        self.next_page_id += 1;
+        return page_id;
     }
 
-    pub fn allocatePage() Page {
-        var page_id = Self.next_page_id;
-        Self.next_page_id += 1;
-        return Page{.id=page_id};
-    }
-
-    pub fn write_page_data(page: Page, data: []const u8) !void {
+    pub fn write_page_data(self: *Self, page_id: usize, data: []const u8) !void {
         // オフセットを計算
-        var offset = PAGE_SIZE * page.id;
+        const offset = PAGE_SIZE * page_id;
         // ページ先頭へシーク
-        Self.heap_file.seekTo(offset);
+        try self.heap_file.seekTo(offset);
         // データを書き込む
-        Self.heap_file.write(data);
+        try self.heap_file.writeAll(data);
     }
 
-    pub fn read_page_data(page: Page, data: []const u8) !void {
+    pub fn read_page_data(self: *Self, page_id: usize, data: []u8) !usize {
         // オフセットを計算
-        var offset = PAGE_SIZE * page.id;
+        const offset = PAGE_SIZE * page_id;
         // ページ先頭へシーク
-        Self.heap_file.seekTo(offset);
+        try self.heap_file.seekTo(offset);
+
+        // ファイルが読み取り用に開かれていることを確認
+        const access_mode = try self.heap_file.getAccessMode();
+        if (access_mode & .readable == 0) {
+            return error.NotOpenForReading;
+        }
+
         // データを読み出す
-        Self.heap_file.read(data);
+        return try self.heap_file.read(data);
     }
 };
 
-test "Page" {
-    var page1 = Page{.id=1};
-    var page11 = Page{.id=1};
-    try std.testing.expectEqual(page1, page11);
-}
+test "test DiskManager" {
+    // ファイル名
+    const heap_file_path = "test_heap_file.db";
+    defer std.fs.cwd().deleteFile(heap_file_path) catch {};
 
-test "createOrOpen" {
-    var file =  createOrOpen("/dev/null");
-    try std.testing.expectEqual(@TypeOf(file), File.OpenError!File);
-}
+    // DiskManagerを開く
+    var dm = try DiskManager.open(heap_file_path);
 
-test "Disk.open" {
-    const dm = DiskManager;
-    var heap = dm.open("/dev/null");
-    try std.testing.expectEqual(@TypeOf(heap), File.OpenError!DiskManager);
-}
+    // ページを割り当てて、書き込み、読み出し
+    const page = try dm.allocate_page();
+    const data: []const u8 = "Hello, world!";
+    try dm.write_page_data(page, data);
+    var buf: [PAGE_SIZE]u8 = undefined;
+    var bytes_read = try dm.read_page_data(page, buf[0..]);
+    try expectEqual(bytes_read, data.len);
+    try expectEqual(data, buf[0..bytes_read]);
 
-test "Disk.allocatePage" {
-    const dm = DiskManager;
-    var heap = try dm.open("/dev/null");
-    var page = heap.allocatePage();
-    try std.testing.expectEqual(@TypeOf(page), Page);
+    // 2つめのページを割り当て、書き込み、読み出し
+    const page2 = try dm.allocate_page();
+    const data2: []const u8 = "Goodbye, world!";
+    try dm.write_page_data(page2, data2);
+    var buf2: [data2.len]u8 = undefined;
+    const bytes_read2 = try dm.read_page_data(page2, buf2[0..]);
+    try expectEqual(bytes_read2, data2.len);
+    try expectEqual(data2, buf2[0..bytes_read2]);
 }
